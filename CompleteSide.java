20c16dae827f75f03e1813e37d565a8bf7653a5c
HastenX <133537562+HastenX@users.noreply.github.com>
2025-12-07 22:04:28 -0500

    Update CompleteSide.java

--- CompleteSide.java
+++ CompleteSide.java
@@ -14,7 +14,7 @@ public class CompleteSide extends Trail{
         for(Trail trail: trails) {
             System.out.println("Weight (g); "+ (trail.totalMass) +":");
             System.out.println("Friction Force (N):  " + Math.abs(((trail.totalMass/1000)*(trail.accel))-((trail.massOfDrag/1000)*Trail.gravityAccel)));
-            System.out.println("Normal Force (N): " + Math.abs(massOfBlock*Trail.gravityAccel));
+            System.out.println("Normal Force (N): " + Math.abs(trail.massOfDrag*Trail.gravityAccel));
             System.out.println("Average Friction coefficent: " + Math.abs(trail.fric));
         }
         System.out.println("Total Average Friction coefficent: " + Math.abs(Trail.averageFriction(trails)));
